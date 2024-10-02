93968ec60bb89a4ac592cae4f7698dccfe6dd895
Andrew Sackey <sackeya24@gmail.com>
2024-10-02 12:11:04 +0000

    my app db commit

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import todoRouter from "./routes/todo.js";
+import userRouter from "./routes/user.js";
+
+// connect to database
+await mongoose.connect('mongodb+srv://todo-api:todo-api@mywebdevdb.f0dmn.mongodb.net/todo-db?retryWrites=true&w=majority&appName=mywebdevDB');
 
 // create an express app
 const app = express();
 
-// Define routes
-app.get("/hello", function (req, res, next) {
-  console.log(req.headers);
-  res.json("You viisited the hello endpoint");
-});
-
-app.get("/goodbye", function (req, res, next) {
-  console.log(req.query);
-  res.json("adios, see you again");
-}); 
+// Use routes
+app.use(todoRouter);
+app.use(userRouter);
 
 // Listen for incoming requests
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log("App is listening on port 3000");
 });
 
